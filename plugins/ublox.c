fba1ee5b79c0f7ea5d6fed35a620b5151b1e3fdf
Dragos Tatulea <dragos@endocode.com>
2016-02-04 14:20:45 +0100

    ublox: wait for the modem to wake up
    The Toby L2 can take a considerable amount of time to wake up when
    just plugged in.

--- plugins/ublox.c
+++ plugins/ublox.c
@@ -201,6 +201,9 @@ static int ublox_enable(struct ofono_modem *modem)
 
 	}
 
+	/* The modem can take a while to wake up if just powered on. */
+	g_at_chat_set_wakeup_command(data->aux, "AT\r", 1000, 11000);
+
 	g_at_chat_send(data->aux, "ATE0 +CMEE=1", none_prefix,
 					NULL, NULL, NULL);
 
